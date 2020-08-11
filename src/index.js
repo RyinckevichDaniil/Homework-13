bf360b2445c8f094f491c3809090e89dd68f0486
RyinckevichDaniil <abdul.alahakbar@gmail.com>
2020-08-11 19:28:09 +0300

    ok

--- src/index.js
+++ src/index.js
@@ -1,69 +1,3 @@
-/* const toDo = {
-    notes: [
-            {value: "Js", completed: false, id: 1596559757142},
-            {value: "World", completed: false, id: 1596559757043},
-            {value: "Hello", completed: false, id: 1596559756940},
-    ],
-    addNote(value) {
-        const note = {
-            value,
-            completed: false,
-            id: Date.now(),
-        };
-
-        this.notes = [note, ...this.notes];
-    },
-
-    removeNote(id, confirm) {
-        if (confirm) {
-            this.notes = this.notes.filter(note => note.id !== id);
-        }
-    },
-
-    completeNote(id) {
-        this.notes = this.notes.map(note => ({
-            ...note,
-            completed: note.id === id ? !note.completed : note.completed
-        }))
-    },
-
-    editNote(id, value, confirm) {
-        if (confirm) {
-            this.notes = this.notes.map(note => {
-                let newNote = note;
-                
-                if (note.id === id) {
-                    newNote = {
-                        ...note,
-                        value
-                    }
-                };
-
-                return newNote;
-            });
-        }
-    },
-
-    get statistic() {
-        return this.notes.reduce(
-            (acc, note) => {
-                note.completed && acc.completed++;
-                return acc;
-            },
-            { total: this.notes.length, completed: 0 }
-            );
-    }
-};
-
-Object.freeze(toDo);
-
-/* toDo.addNote('Hello');
-setTimeout(() => toDo.addNote('World'), 100);
-setTimeout(() => toDo.addNote('Js'), 200); */
-/* toDo.editNote(1596559756940, 'World', confirm('?'));
-
-console.log(toDo); */
-
 const toDoList = function () {
     this.notes = [
         { value: "Js", completed: false, id: 1596559757142 },
@@ -79,9 +13,7 @@ toDoList.prototype.addNote = function (value) {
         completed: false,
         id: Date.now(),
     };
-
     this.notes = [note, ...this.notes];
-    console.log(this.notes)
 };
 
 toDoList.prototype.removeNote = function (id, confirm) {
@@ -126,12 +58,12 @@ toDoList.prototype.getStatistic = function () {
 
 const myToDoList = new toDoList();
 
-console.log(myToDoList);
+/* console.log(myToDoList);
 console.log(myToDoList.addNote('Valera'));
 console.log(myToDoList.editNote(1596559756940, 'JavaS', true));
 console.log(myToDoList.removeNote(1596559757142, true));
 
-console.log(myToDoList);
+console.log(myToDoList); */
 
 
 
